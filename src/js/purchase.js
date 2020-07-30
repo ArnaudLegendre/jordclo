2cc0c23f9c230e66508d814d44ed691042b045f9
Andre LECLERCQ <andreleclercqpro@gmail.com>
2020-07-30 16:26:39 +0200

    HotFix 0.3.2

--- src/js/purchase.js
+++ src/js/purchase.js
@@ -1,93 +1,93 @@
-document.addEventListener( 'initWebsite', ( ) => {
+document.addEventListener('initWebsite', () => {
 
-    document.getElementById('purchase' ) ? purchase( 'step1' ) : null
+    document.getElementById('purchase') ? purchase('step1') : null
 
-} )
-document.addEventListener( 'pageChange', ( ) => {
+})
+document.addEventListener('pageChange', () => {
 
-    document.getElementById('purchase' ) ? purchase( 'step1' ) : null
+    document.getElementById('purchase') ? purchase('step1') : null
 
-} )
+})
 
-function purchase( step ){
+function purchase(step) {
 
-    let purchaseElem = document.getElementById('purchase' )
-    let content = purchaseElem.querySelector('.content' )
+    let purchaseElem = document.getElementById('purchase')
+    let content = purchaseElem.querySelector('.content')
     let purchaseBtn
 
-    if ( step === 'step1' ) {
+    if (step === 'step1') {
         content.innerHTML = cartHTML
-        purchaseBtn = content.querySelector('.purchaseButton' )
+        purchaseBtn = content.querySelector('.purchaseButton')
         purchaseBtn.firstElementChild.innerHTML = "Continuer ma commande"
         purchaseBtn.href = "#commander?etape=2"
-        refreshCart( )
+        refreshCart()
 
-    } else if ( step === 'step2' ) {
+    } else if (step === 'step2') {
         content.innerHTML = userProfilHTML
-        purchaseBtn = content.querySelector('.purchaseButton' )
+        purchaseBtn = content.querySelector('.purchaseButton')
         purchaseBtn.hidden = false
         purchaseBtn.firstElementChild.innerHTML = "Finaliser ma commande"
         purchaseBtn.href = "#commander?etape=3"
-        content.querySelector('.editPassword' ).style.display = 'none'
-        getUserProfilPage( content )
+        content.querySelector('.editPassword').style.display = 'none'
+        getUserProfilPage(content)
 
-    } else if ( step === 'step3' ) {
-        let allLabelSpan = document.querySelectorAll('.labelSpan' )
-        purchaseBtn = content.querySelector('.purchaseButton' )
+    } else if (step === 'step3') {
+        let allLabelSpan = document.querySelectorAll('.labelSpan')
+        purchaseBtn = content.querySelector('.purchaseButton')
         const isNotEmpty = elem => elem > 0
-        let labelSpanArray = [ ]
+        let labelSpanArray = []
         allLabelSpan.forEach(elt => {
-            labelSpanArray.push( elt.innerHTML.length )
-        } )
-        labelSpanArray.every( isNotEmpty ) ? createOrders() : showPushNotification( 'error', "Veuillez remplir tous les champs" )
-    } else if ( step === 'step4' ){
+            labelSpanArray.push(elt.innerHTML.length)
+        })
+        labelSpanArray.every(isNotEmpty) ? createOrders() : showPushNotification('error', "Veuillez remplir tous les champs")
+    } else if (step === 'step4') {
 
         content.innerHTML = shippingHTML
-        purchaseBtn = content.querySelector('.purchaseButton' )
+        purchaseBtn = content.querySelector('.purchaseButton')
         purchaseBtn.firstElementChild.innerHTML = "Procéder au paiement"
         purchaseBtn.href = "#commander?etape=5"
-        calcShipping( document.getElementById('cartModal').querySelector('.cartPrice').innerHTML )
+        calcShipping(document.getElementById('cartModal').querySelector('.cartPrice').innerHTML)
 
-    } else if ( step === 'step5' ){
+    } else if (step === 'step5') {
 
         content.innerHTML = paymentHTML
 
     }
 
-    if( purchaseBtn ) {
+    if (purchaseBtn) {
 
         purchaseBtn.addEventListener('click', e => {
 
-            e.preventDefault( )
+            e.preventDefault()
 
-            if ( localStorage.getItem('userLocal' ) ) {
+            if (localStorage.getItem('userLocal')) {
 
-                e.target.closest( '.purchaseButton' ).hash === '#commander?etape=2' ? purchase( 'step2' ) : null
-                e.target.closest( '.purchaseButton' ).hash === '#commander?etape=3' ? purchase( 'step3' ) : null
-                e.target.closest( '.purchaseButton' ).hash === '#commander?etape=5' ? purchase( 'step5' ) : null
+                e.target.closest('.purchaseButton').hash === '#commander?etape=2' ? purchase('step2') : null
+                e.target.closest('.purchaseButton').hash === '#commander?etape=3' ? purchase('step3') : null
+                e.target.closest('.purchaseButton').hash === '#commander?etape=5' ? purchase('step5') : null
 
-            }  else {
+            } else {
 
                 content.innerHTML = loginLogoutFormHTML
-                loginRegister( 'purchase' )
+                loginRegister('purchase')
 
             }
 
-        } )
+        })
     }
 }
 
-function createOrders( ) {
+function createOrders() {
 
-    let userLocal = JSON.parse( localStorage.getItem('userLocal' ) )
+    let userLocal = JSON.parse(localStorage.getItem('userLocal'))
     let emailUser = userLocal.email
     let tokenUser = userLocal.token
 
-    fetch( `/api?action=orders&token=${tokenUser}&email=${emailUser}` )
-        .then( res => {
-            return res.json( )
-        } ).then( data => {
-        data === 'create document' ? purchase( 'step4' ) : showPushNotification( 'error', "Une erreur est survenue, merci de contacter l'adminitrateur" )
-    } )
+    fetch(`/api?action=orders&token=${tokenUser}&email=${emailUser}`)
+        .then(res => {
+            return res.json()
+        }).then(data => {
+        data === 'create document' ? purchase('step4') : showPushNotification('error', "Une erreur est survenue, merci de contacter l'adminitrateur")
+    })
 
 }
--- src/js/router.js
+++ src/js/router.js
@@ -1,152 +1,163 @@
-let routeList = [ ]
+let routeList = []
 let route
 let currentPage
-let routes = { }
+let routes = {}
 
-;( ( ) => { fetch( '/api?action=get&name=pages' )
+;(() => {
+    fetch('/api?action=get&name=pages')
 
-    .then( res => { return res.json( ) } )
-
-    .then( data => {
-
-        let folder = 'assets/views/pages/'
-
-        data.forEach( e => {
-            let newPage = {
-                'slug': e.slug,
-                'fileName': folder + e.fileName + '.html',
-                'title': e.title,
-                'access': e.access,
-            }
-            routeList.push( newPage )
+        .then(res => {
+            return res.json()
         })
 
-        Object.assign( routes, routeList )
+        .then(data => {
 
-        loadProducts( )
+            let folder = 'assets/views/pages/'
 
-    } )
+            data.forEach(e => {
+                let newPage = {
+                    'slug': e.slug,
+                    'fileName': folder + e.fileName + '.html',
+                    'title': e.title,
+                    'access': e.access,
+                }
+                routeList.push(newPage)
+            })
 
-} )( );
+            Object.assign(routes, routeList)
 
-function loadProducts( ) { fetch( '/api?action=get&name=products' )
+            loadProducts()
 
-    .then( res => { return res.json( ) } )
-
-    .then( data => {
-
-        let folder = 'assets/views/templates/'
-
-        data.forEach( e => {
-            let newPage = {
-                'slug': e.slug,
-                'fileName': folder + 'product.html',
-                'title': e.name,
-                'access': e.access,
-            }
-            routeList.push( newPage )
         })
 
-        Object.assign( routes, routeList )
+})();
 
-        document.dispatchEvent( dbReady )
+function loadProducts() {
+    fetch('/api?action=get&name=products')
 
-        localStorage.setItem( 'products', JSON.stringify( data ) )
+        .then(res => {
+            return res.json()
+        })
 
-    } )
+        .then(data => {
+
+            let folder = 'assets/views/templates/'
+
+            data.forEach(e => {
+                let newPage = {
+                    'slug': e.slug,
+                    'fileName': folder + 'product.html',
+                    'title': e.name,
+                    'access': e.access,
+                }
+                routeList.push(newPage)
+            })
+
+            Object.assign(routes, routeList)
+
+            document.dispatchEvent(dbReady)
+
+            localStorage.setItem('products', JSON.stringify(data))
+
+        })
 
 };
 
 class Router {
 
-    constructor( routes ) {
+    constructor(routes) {
 
         this.routes = routes
-        this.cache = { }
+        this.cache = {}
 
-        window.addEventListener( 'hashchange', this.loadPage.bind( this, 'hashchange' ) )
-        document.addEventListener( 'dbReady', this.loadPage.bind( this, 'dbReady' ) )
+        window.addEventListener('hashchange', this.loadPage.bind(this, 'hashchange'))
+        document.addEventListener('dbReady', this.loadPage.bind(this, 'dbReady'))
     }
 
-    async loadPage( e, event ){
+    async loadPage(e, event) {
 
         route = location.hash || '#'
-        currentPage = await Object.values( this.routes ).find( elt => route === `#${ elt.slug }` )
+        currentPage = await Object.values(this.routes).find(elt => route === `#${elt.slug}`)
 
-        if( currentPage === undefined ){
+        if (currentPage === undefined) {
 
             route = '#404'
-            currentPage = Object.values( this.routes ).find( elt => `#${ elt.slug }` === '#404' )
-            showPage.bind( this )( )
+            currentPage = Object.values(this.routes).find(elt => `#${elt.slug}` === '#404')
+            showPage.bind(this)()
 
         } else {
 
-            if( currentPage.access === '1' ){
+            if (currentPage.access === '1') {
 
-                let userLocal = localStorage.getItem('userLocal' )
+                let userLocal = localStorage.getItem('userLocal')
 
-                if( userLocal != null ) {
+                if (userLocal != null) {
 
                     userLocal = JSON.parse(userLocal)
                     let userToken = userLocal.token;
 
-                    ( ( ) => { fetch(`/api?action=token&token=${userToken}&state=verify` )
-                        .then( res => { return res.json( ) } )
-                        .then( data => {
-                            if ( data != true ) {
-                                route = '#401'
-                                currentPage = Object.values( this.routes ).find(elt => `#${elt.slug}` === '#401' )
-                                showPage.bind( this )( )
-                                localStorage.removeItem( 'userLocal' )
-                            } else {
-                                showPage.bind( this )( )
-                            }
-                        } )
-                    } )( )
+                    (() => {
+                        fetch(`/api?action=token&token=${userToken}&state=verify`)
+                            .then(res => {
+                                return res.json()
+                            })
+                            .then(data => {
+                                if (data != true) {
+                                    route = '#401'
+                                    currentPage = Object.values(this.routes).find(elt => `#${elt.slug}` === '#401')
+                                    showPage.bind(this)()
+                                    localStorage.removeItem('userLocal')
+                                } else {
+                                    showPage.bind(this)()
+                                }
+                            })
+                    })()
 
                 } else {
                     route = '#401'
-                    currentPage = Object.values( this.routes ).find(elt => `#${elt.slug}` === '#401' )
-                    showPage.bind( this )( )
+                    currentPage = Object.values(this.routes).find(elt => `#${elt.slug}` === '#401')
+                    showPage.bind(this)()
                 }
             } else {
-                showPage.bind( this )( )
+                showPage.bind(this)()
             }
 
 
         }
 
-        async function showPage(  ) {
+        async function showPage() {
 
 
-            if( !this.cache.hasOwnProperty( route ) ) {
+            if (!this.cache.hasOwnProperty(route)) {
 
-                let res = await fetch( currentPage.fileName )
+                let res = await fetch(currentPage.fileName)
                 this.cache[route] = await res.text()
 
             }
 
-            let newRoute = route.replace( '#', '/' )
+            let newRoute = route.replace('#', '/')
 
-            history.replaceState( this.cache[route], null, newRoute )
+            history.replaceState(this.cache[route], null, newRoute)
 
-            document.getElementById( 'content' ).innerHTML = this.cache[route]
+            document.getElementById('content').innerHTML = this.cache[route]
 
             document.querySelector('title').innerHTML = currentPage.title
 
-            if( event.type === 'dbReady' )
-                document.dispatchEvent( pageReady )
+            if (event.type === 'dbReady')
+                document.dispatchEvent(pageReady)
 
         }
     }
 }
 
-let pagesRoutes = new Router( routes )
+let pagesRoutes = new Router(routes)
 
 let currentPageAccess
 window.onpopstate = e => {
 
-    document.getElementById( 'content' ).innerHTML = pagesRoutes.cache[ document.location.pathname.replace( '/', '#' ) ]
-    setTimeout(( ) => { document.dispatchEvent( pageChange ) }, 200)
+    document.getElementById('content').innerHTML = pagesRoutes.cache[document.location.pathname.replace('/', '#')]
+    setTimeout(() => {
+        document.dispatchEvent(pageChange)
+    }, 200)
 
 }
